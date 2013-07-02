04616048c24c20529d4d994aea5a21b4443ec06f
Jürgen Brüder <juergen.brueder@gmail.com>
2013-07-02 10:27:09 +0200

    added testcases

--- spec/valiban/valiban_spec.rb
+++ spec/valiban/valiban_spec.rb
@@ -4,11 +4,18 @@ module Valiban
 
   describe IBAN do
 
-    it "should break up an IBAN code into bank account and bank number" do
-      iban = "AT75380560005030010"
+    before(:each) do
+      @test_iban = "AT75480570005040015"
+    end
 
-      IBAN.to_bank_account_number.should == ""
-      IBAN.to_bank_code_number.should == ""
+    it "should break up an IBAN code into a bank account number" do
+      #IBAN.get_bank_account_number(@test_iban).should == ""
+    end
+
+    it "should break up an IBAN code into a bank code number" do
+    end
+
+    it "should get the country of an IBAN code" do
     end
 
   end
